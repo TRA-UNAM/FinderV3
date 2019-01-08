4d5283c79b76c49ff762a0091027894a41eec9ba
mavro10600 <maurofimec@gmail.com>
2019-01-08 14:16:06 -0600

    gitignore

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/mavro/FinderV3/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/mavro/FinderV3/catkin_ws/devel;/home/mavro/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/mavro/FinderV3/catkin_ws/devel;/home/mavro/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- catkin_ws/build/finder_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mavro/FinderV3/catkin_ws/src/finder_bringup
-# Build directory: /home/mavro/FinderV3/catkin_ws/build/finder_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
