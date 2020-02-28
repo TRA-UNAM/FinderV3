00db6fc84c952f338f3affa12204c3ba1e93c3be
Axel Rojas <axeljavier13.rm@gmail.com>
2020-02-28 14:25:52 -0600

    Se agregaron comandos para la descarga de Opencv

--- finder_setup.sh
+++ finder_setup.sh
@@ -6,14 +6,13 @@
 sudo apt-get update
 sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install python3.6-dev
-echo "Check the following paths..."
+echo "Check the following paths...(need to be the same)"
 python3.6-config --includes
-cd ~/Desktop/
+cd ~/
 mkdir OpenCV-tmp
 cd OpenCV-tmp
 git clone https://github.com/Itseez/opencv.git
-#Agregar aqui comando para renombrar el directorio "opencv" a "opencv-3"
-echo "Rename from opencv to opencv-3"
+mv opencv opencv-3
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ../opencv-3
@@ -21,4 +20,7 @@ make -j $(nproc --all)
 sudo make install
 sudo apt-get install python3-numpy
 sudo apt-get install python3-matplotlib
+cd ~/
+echo "done"
 
+sudo apt-get install ros-melodic-cv-camera
