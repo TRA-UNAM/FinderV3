23c88d78926066e0c6c73d5081600e5bc8802c0f
Roberto <garg.roberto@gmail.com>
2020-05-23 20:06:18 -0500

    Corrección de las librerías para visión

--- finder_setup.sh
+++ finder_setup.sh
@@ -6,24 +6,19 @@
 sudo apt-get update
 sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install python3.6-dev
-echo "Check the following paths...(need to be the same)"
-python3.6-config --includes
-cd ~/
-mkdir OpenCV-tmp
-cd OpenCV-tmp
-git clone https://github.com/Itseez/opencv.git
-mv opencv opencv-3
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ../opencv-3
-make -j $(nproc --all)
-sudo make install
+
+#Librerías para visión
+sudo apt-get install opencv-data
+sudo apt-get install python-opencv
+sudo apt-get install python3-opencv
+sudo apt-get install ros-melodic-cv-camera
+#Librería código QR
+sudo apt-get install python-zbar
+
 sudo apt-get install python3-numpy
 sudo apt-get install python3-matplotlib
 cd ~/
 echo "done"
-
-sudo apt-get install ros-melodic-cv-camera
 
 #File to install the eBUS_SDK for thermal camera (Preguntar a Nacho sobre el proceso completo)
 cd ~/FinderV3/ToInstall/
