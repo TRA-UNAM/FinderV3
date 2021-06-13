6fe724ddb12386b0a31bc071c5a5c4aa66b108f4
AxelRojas <AxelRojas@github.com>
2021-06-12 21:02:13 -0500

    Se siguio trabajando en el nodo de exploracion, ya se logro obtener la posicion del robot y se trabajara en el filtrado de puntos objetivo, para despues generar la ruta

--- catkin_ws/src/navigation/exploration/src/scripts/Metodos_de_navegacion_autonoma.py
+++ catkin_ws/src/navigation/exploration/src/scripts/Metodos_de_navegacion_autonoma.py
@@ -10,10 +10,14 @@ from nav_msgs.msg import OccupancyGrid
 from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
+from nav_msgs.msg import Odometry
+import heapq
+
 #El mapa de Inflado 2 infla todos los puntos alrededor de un obstaculo el número de celdas que le digas
 def mapa_inflado_2(self,num_celdas_inflar,grafo):
+
+    print ("Calculando el mapa inflando " +str(num_celdas_inflar) + " cells\n")
     pub_inflated=rospy.Publisher("/inflated_map", OccupancyGrid, queue_size=10)
-    num_celdas_inflar=int(num_celdas_inflar/self.dato.info.resolution)
     c, l=np.shape(grafo)
     mapa_inflado=np.copy(grafo)
     #Voy a inflar el mapa para poder obtener las rutas y que el robot se aleje de las paredes del mapa, entonces sobre el original cada vez que me encuentre 
@@ -25,7 +29,7 @@ def mapa_inflado_2(self,num_celdas_inflar,grafo):
                     for k2 in range(j-num_celdas_inflar,j+num_celdas_inflar+1):#Aqui para inflar las columnas
                         mapa_inflado[k1,k2]=grafo[i,j]#Con k1 y k2 delimito el cuadrado que voy a llenar y marco como ocupada el cuadro al rededor del punto segun el numero de celdas que desean
     
-    print("Acabe de inflar el mapa, ahora calculare los puntos objetivo")  
+    print("Acabe de inflar el mapa\n")  
     #Voy a publicar el mapa inflado para observarlo, por ello debo poner otro mapa
     mapa_ocupacion = OccupancyGrid()
     mapa_ocupacion.header.seq=self.dato.header.seq
@@ -48,8 +52,7 @@ def mapa_inflado_2(self,num_celdas_inflar,grafo):
     return mapa_inflado
 #El mapa de Inflado 1 infla solo los puntos que se encuentran al lado de espacio conocido, pero requiere muchas más operaciones
 def mapa_inflado_1(self,num_celdas_inflar,grafo):
-    pub_inflated=rospy.Publisher("/inflated_map", OccupancyGrid, queue_size=10)
-    num_celdas_inflar=int(num_celdas_inflar/self.dato.info.resolution)
+    print ("Calculando el mapa inflando " +str(num_celdas_inflar) + " cells\n")
     c, l=np.shape(grafo)
     mapa_inflado=np.copy(grafo)
     #Voy a inflar el mapa para poder obtener las rutas y que el robot se aleje de las paredes del mapa, entonces sobre el original cada vez que me encuentre 
@@ -312,7 +315,7 @@ def mapa_inflado_1(self,num_celdas_inflar,grafo):
                     else:
                         mapa_inflado[i:i+num_celdas_inflar,j]=100  
                         
-    print("Acabe de inflar el mapa, ahora calculare los puntos objetivo")  
+    print("Acabe de inflar el mapa\n")  
     #Voy a publicar el mapa inflado para observarlo, por ello debo poner otro mapa
     mapa_ocupacion = OccupancyGrid()
     mapa_ocupacion.header.seq=self.dato.header.seq
@@ -332,10 +335,25 @@ def mapa_inflado_1(self,num_celdas_inflar,grafo):
     pub_inflated.publish(mapa_ocupacion)
     return mapa_inflado
 
-#def mapa_de_costos(radio_de_costos,grafo):
+def mapa_de_costos(self,num_celdas_costo,grafo):
+    
+    print ("Caculando el mapa de costos con " +str(num_celdas_costo) + " celdas\n")
+    cost_map = np.copy(grafo)
+    [height, width] = grafo.shape
+    
+    for i in range(height):
+        for j in range(width):
+            if grafo[i,j]==100:#checamos para cada punto del mapa si esta ocupado 
+                for k1 in range(-num_celdas_costo,num_celdas_costo+1):#Voy a ir de menos el radio hasta más el radio
+                    for k2 in range(-num_celdas_costo,num_celdas_costo+1):#Aqui para inflar las columnas
+                        cost=num_celdas_costo-max(abs(k1),abs(k2))#Se resta el radio de costo a la cordenada en valor absoluto mayor 
+                        if cost>cost_map[i+k1,i+k2]:#Si el costo es mayor a el valor que tenia la celda entonces si la pongo, si no es así, se queda igual
+                            cost_map[i+k1,i+k2]=cost#Con k1 y k2 delimito el cuadrado que voy a llenar y voy a tener que comparar para ver si sustituyo
     
     
-              
+                    
+    return cost_map
+          
 def convertir_matriz(grafo):
     """#Entra un grafo de 40x40 que es subgrafo que envuelve al robot en un area cuadrada, en la cual se hara la busqueda de objetivos.
     #Este grafo fue modificado colocando como 1 los valores conocidos, esto porque se consideran grafos conectados, y a los demas valores se les ha colocado un 0.
@@ -471,7 +489,6 @@ def Busqueda_Objetivos(self,numero_de_celdas,grafo):
 
     c, l=np.shape(grafo)
     nodos_objetivo=[]
-    grafo=mapa_inflado_2(self,numero_de_celdas, grafo)
     
     for j in range(l):
         for i in range(c): 
@@ -667,15 +684,22 @@ def Busqueda_Objetivos(self,numero_de_celdas,grafo):
                         nodos_objetivo.append((i-1,j))
 
 
-    print("Ya termine de calcular los puntos objetivo")
-    return grafo,nodos_objetivo 
-
- 
-      
+    print("Ya termine de calcular los puntos objetivo\n")
+    print("Encontre un total de "+str(len(nodos_objetivo))+" candidatos\n")
+    return nodos_objetivo      
 #Me permite visualizar el número de puntos objetivos encontrados en el mapa a explorar
-def visualizacion_objetivos(objetivos,mapa):
+def Filtrado_de_objetivos(objetivos,mapa_de_costos):
+    costos=[]
+    for [x,y] in objetivos:
+        heapq.heappush(costos,(mapa_de_costos[x,y],(x,y)))#Recorro todos los puntos y los voy colocando de menor a mayor costo
     
-    pub =rospy.Publisher('/visualization_marker',Marker, queue_size=50)
+    
+    return costos
+     
+def visualizacion_objetivos(self,objetivos,mapa):
+    
+    
+    pub =rospy.Publisher('/visualization_marker',Marker, queue_size=100)
     puntos=Marker(ns="puntos_objetivo",type=Marker.POINTS,action=Marker.ADD,lifetime=rospy.Duration(),id=0)
     puntos.header.stamp=rospy.Time()
     puntos.header.frame_id="/map"
@@ -685,27 +709,87 @@ def visualizacion_objetivos(objetivos,mapa):
     puntos.pose.position.y=mapa.info.origin.position.y+0.025
     puntos.pose.position.z=0
     #Puntos
-    puntos.scale.x=0.04
-    puntos.scale.y=0.04
-    #Los puntos seran rojos
-    puntos.color.g=1.0
-    puntos.color.a=1.0
+    puntos.scale.x=0.1#Tamaño de los puntos
+    puntos.scale.y=0.1
+    puntos.scale.z=0.1
+    #Los puntos seran verdes
+    puntos.color.g=1.0#Color 
+    puntos.color.a=1.0#Nitidez
+
     
-    """o=Point()
-    o.x=0
-    o.y=0
+    o=Point()
+    o.x=self.pos_x_robot*mapa.info.resolution
+    o.y=self.pos_y_robot*mapa.info.resolution
     o.z=0
-    puntos.points.append(o)"""
-    #Son las posiciones de los puntos
+    puntos.points.append(o)
+    
     
     for (x,y) in objetivos:
         p=Point()
+        
         p.x=y*(mapa.info.resolution)#Se hizo una regla de tres o se multiplico por la resolución del mapa para ajustar los valores de la matriz a los valores de 
         p.y=x*(mapa.info.resolution)
         p.z=0
         puntos.points.append(p)
     
+    rate=rospy.Rate(20)
     
-    rate = rospy.Rate(20)#defino que los datos se publicaran 15/s
     pub.publish(puntos)
     rate.sleep()
+    
+def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
+    #
+    # Write a A* algorithm to find a path in an occupancy grid map given the start cell
+    # [start_r, start_c], the goal cell [goal_r, goal_c] and the map 'grid_map'.
+    # Return the set of points of the form [[start_r, start_c], [r1,c1], [r2,c2], ..., [goal_r, goal_c]]
+    # If path cannot be found, return an empty tuple []
+    # Use Manhattan distance as heuristic function
+    # Hint: Use a priority queue to implement the open list
+    # Documentation to implement priority queues in python can be found in
+    # https://docs.python.org/2/library/heapq.html
+    #
+    g_values=np.full(np.shape(grid_map),sys.maxint)#Creo un arreglo para almacenar todas las g del mapa y lo lleno de valores muy grandes inicialmente para poder comparar e ir agregando las g, en este caso me sirve para poder ir llenando las g de los primeros puntos
+    f_values=np.full(np.shape(grid_map),sys.maxint)#Creo un arreglo para almacenar todas las f del mapa y lo lleno de valores muy grandes inicialmente para poder comparar e ir agregando las f, en este caso me sirve para poder ir llenando las f de los primeros puntos
+    p_nodes=np.full((np.shape(grid_map)[0],np.shape(grid_map)[1],2),-1)#Creo la lista de puntos anteriores para cada nodo con 2 dimensiones y la tercera con un valor de 2 ya que no lo necesitaremos, y los inicio con un valor de -1 para saber el limite
+    in_open_list=np.full(np.shape(grid_map),False)#Estoy creando los verificadores de si esta en la lista cerrada o abierta y los llena de puros Falsos
+    in_close_list=np.full(np.shape(grid_map),False)#Lista de puntos visitados
+    
+    open_list=[]#lista de puntos candidatos
+    heapq.heappush(open_list,(0,[start_r,start_c]))#Agrego a la lista en la posicion 0 el punto inicial
+    g_values[start_r,start_c]=0
+    f_values[start_r,start_c]=0
+    in_open_list[start_r,start_c]=True
+    [r,c]=[start_r,start_c]#r,c sera el nodo actual, iniciando por la posicion del robot
+    steps=0
+    path=[]
+    while len(open_list)>0 and [r,c]!=[goal_r,goal_c]:
+        [r,c]=heapq.heappop(open_list)[1]#Estoy sacando el elemento con mayor prioridad y como me regresa la prioridad y el elemento entonces me interesa saolo sacar el elemento que es el 2 del arreglo
+        in_close_list[r,c]=True#Decimos que ya visitamos el nodo que acabo de extraer 
+        neighbors=[[r+1,c],[r-1,c],[r,c+1],[r,c-1]]#Sacamos los vecinos del punto actual
+        for [nr,nc]in neighbors:#Para cada uno de los vecinos
+            if grid_map[nr,nc]!=0 or in_close_list[nr,nc]:#Si el nodo vecino esta ocupado=100 o es desconocido=-1 o ya fue visitado se omite
+                continue
+            g=g_values[r,c]+1+cost_map[r,c]#Genero la g del punto anterios más 1, en este caso no es g=0 porque lo llene con un valor muy grande, pero no importa, ya que tiene el mismo efecto que empezar con g=0 y le agrego ademas una función de costos que es la distancia que hay de puntos a obstaculos ya que no quiero que el robot se acerque a obstaculos
+            f=g+(np.abs(goal_r-nr)+np.abs(goal_c-nc))#Calculo la f con la g y la distancia de manhatan
+            if f<f_values[nr,nc]:#Si el costo del nodo vecino de r,c es menor al costo del nodo vecino nr, nc entonces cambio la g del nodo vecino, ya que con ello digo que al nodo vecino llegue por otra ruta mejor
+                g_values[nr,nc]=g #Aqui lo que hago es que yo llegue por una ruta al punto nr, nc y si la g fue menor que la g que ya se tenia, tal vez por otra ruta, eso quiere decir que la ruta nueva es mejor y debo cambiar esa g
+                f_values[nr,nc]=f #Aqui lo que hago es que yo llegue por una ruta al punto nr, nc y si la g fue menor que la g que ya se tenia, tal vez por otra ruta, eso quiere decir que la ruta nueva es mejor y debo cambiar esa g
+                p_nodes[nr,nc]=[r,c]#Aquí estoy agregando el predecesor del vecino si se encuentra que el costo es menor que el costo ya calculado por otra ruta tal vez
+            if not in_open_list[nr,nc]: #Si el punto candidato no esta en la lista abierta entonces le cambio el valor, con esto podria agregar dicho punto a la lista abierta para buscar en ellos
+                in_open_list[nr,nc]=True
+                heapq.heappush(open_list,(f,[nr,nc]))#Agrego el punto candidato a la lista abierta si es que no estaba ya y le agrego la g para ordenar de menor g a mayor g e irlos sacando en ese orden
+                
+            steps+=1#Aumento el número de pasos que le tomo al algoritmo calcular la ruta y vuelvo a hacer lo mismo para cada punto de la lista abierta hasta terminar
+       
+    if[r,c]!=[goal_r,goal_c]:#Una vez checo para todos losn nodos, si el valor de r,c no es igual al objetivo es que no hay solucion
+        print("Cannot calculate path by Dijsktra :c")
+        return[]
+    
+    #Aqui r, c ya se quedo con los valores meta, por lo cual el punto anterior
+    #Preguntar sobre p_nodes
+    while [p_nodes[r,c][0],p_nodes[r,c][1]]!=[-1,-1]:#Sí si existe la solución entonces hasta encontrar el nodo [-1,-1] que fue con lo que se lleno y me dice que son todos los puntos
+        path.insert(0,[r,c])#Voy agregando en la posicion cero cada una de las coordenadas que es el punto anterior
+        [r,c]=p_nodes[r,c]
+    print("The path has had "+str(steps)+" steps for been calculated")
+    return path    
+   
--- catkin_ws/src/navigation/exploration/src/scripts/Prueba_exploracion.py
+++ catkin_ws/src/navigation/exploration/src/scripts/Prueba_exploracion.py
@@ -5,39 +5,39 @@ import rospy
 #from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 import numpy as np
-from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
 import Metodos_de_navegacion_autonoma as md
 import time
-
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+import heapq
+import math
 
 class Nodo:
     def __init__(self,twist):
         self.init_node=rospy.init_node("Exploration_and_Mapping", anonymous=True)
-        self.laser=[]
         self.twist= twist
         self.dato=0
         self.mapa=0
         self.mapa_inflado=0
+        self.celdas_a_inflar=6#número de celdas a inflar, depende de la resolucion, pero usualmente cada celda son 0.05 m o 5 cm
+        self.celdas_de_costo=3#número de celdas de costo desde el obstaculo, depende de la resolucion, pero usualmente cada celda son 0.05 m o 5 cm
         self.objetivos=[]
-        self.pos_x=0
-        self.pos_y=0
-        self.posicion=0
-        self.nuevo_grafo=0
-        self.distancias=0
-        self.min_x_anterior=400
-        self.min_y_anterior=400
-        self.max_x_anterior=10
-        self.max_y_anterior=10
-        
-        
-        
+        self.pos_x_robot=0
+        self.pos_y_robot=0
+        self.costos=0
+        self.mapa_de_costos=0
 
     def posicion_robot_callback(self,data):
-        #Obtendre la posicion del laser a partir de la posicion de base_footprint
-        self.pos_x=data.pose.position.x
-        self.pos_y=data.pose.position.y
-    
+            #Obtendre la posicion del laser a partir de la posicion de base_footprint
+            
+            pub =rospy.Publisher('/visualization_marker',Marker, queue_size=50)
+            correcion_x=(0.5)*(math.cos(data.pose.pose.orientation.z))#Son las correciones ya que deseo medir desde el laser, y no desde la mitad del robot
+            correcion_y=(0.5)*(math.sin(data.pose.pose.orientation.z))
+            self.pos_y_robot=(self.dato.info.height/2)+((data.pose.pose.position.y+correcion_y)/self.dato.info.resolution)#Estan intercambiados en el mapa, por eso necesito invertirlos
+            self.pos_x_robot=(self.dato.info.width/2)+((data.pose.pose.position.x+correcion_x)/self.dato.info.resolution)#Se ajusta y se suman 4 metros para que este justo en el laser
+
     #Se obtiene un arreglo bidimensional que se transforma en un arreglo bidimensional, en donde las columnas corresponden a las coordenadas en x en el mapa de rviz
     #Mientras que las filas corresponden a las coordenadas en y. Una vez obtenido el mapa, se procede a alterar los valores de la matriz, con el fin de prepararlo tanto para la busqueda de objetivos, como para la evaluacion de distancias
     #Se cambian los valores conocidos (originalmente 0) a valores de 1
@@ -47,9 +47,15 @@ class Nodo:
     #Los puntos necesitan multiplicarse por el factor de escala que es 0.05 aprocimadamente (ya que es la conversion necesaria para pasar de puntos de la matriz a puntos en el mapa)
     #Posteriormente se les resta a estos puntos 10.27 aproximadamente, esto debido a que el origen se encuentra justo en el medio del mapa, y para situarnos en el 0 de la matriz, necesitamos recorrernos -10.27 en ambos ejes (ese valor se obtiene de datos del mapa, y se d¿hace un pequeño ajuste ya que los puntos se ven un poco desfasados)
     #Se publican a una frecuencia de 15 por segundo y en principio despues de esto vendria el filtrado de puntos objetivos
+    
+    
+    
     def map_callback(self,dato):
         #Estoy reacomodando el arreglo para poder manejarlo como una matriz, basandome en la altura y ancho
+        i=0
+        
         self.dato=dato
+        
         #-------------------------------- Estoy preparando una matriz, a partir de todos los puntos que me entrega rviz como arreglo, el elemento 0,0 se encuentra u origen se encuentra en la esquina superior izq, desde ahi, rviz empieza el arreglo que obtenemos en /map
         #Sin embargo, el sistema de referencia del mapa se encuentra justo en el medio, por lo cual debemos de hacer un ajuste para movernos en esta matriz, seria a cada punto restarle la posicion original obtenida del topico /map en origin/posicion y multiplicar los indices por el factor de escala de 0.05
         self.mapa=np.array(self.dato.data).reshape((self.dato.info.height, self.dato.info.width))
@@ -58,31 +64,52 @@ class Nodo:
         self.mapa.flags.writeable = True
         self.mapa[self.mapa==0]=1#Conocido y libre
         self.mapa[self.mapa==-1]=0#Desconocido
-        self.centimetros_a_inflar=0.5 #Los cm son 0.## cm
-        self.mapa_inflado,self.objetivos=md.Busqueda_Objetivos(self,self.centimetros_a_inflar,self.mapa)
+        #Obtengo el mapa inflado
+        self.mapa_inflado=md.mapa_inflado_2(self,self.celdas_a_inflar, self.mapa)
+        #Obtengo el mapa de costos
+        self.mapa_de_costos=md.mapa_de_costos(self,self.celdas_de_costo,self.mapa_inflado)
+        #Obtengo los puntos candidatos
+        self.objetivos=md.Busqueda_Objetivos(self,self.celdas_a_inflar,self.mapa_inflado)
+        #Filtro los puntos segun su costo en el mapa
+        self.objetivos=md.Filtrado_de_objetivos(self.objetivos,self.mapa_de_costos)
+        #Visualizo el punto que salio mejor
         
-        list(set(self.objetivos))#Elimina los elementos repetidos en mis puntos objetivos
-        #md.visualizacion_objetivos(self.objetivos,self.dato)
-        #self.objetivos=md.Filtrado_de_objetivos(self)
-        print(len(self.objetivos))
-        if len(self.objetivos)!=0:
-            md.visualizacion_objetivos(self.objetivos,self.dato)
-        
-        #self.nuevo_grafo=md.convertir_matriz(self.mapa)
-        #self.objetivos=md.calcularDistancias(self)
-        #md.visualizacion_objetivos(self.objetivos,self.dato)
-        #print(distancias_min_cada_objetivo)
-         
 
+        #Aun falta pulir el criterio de seleccion para el punto objetivo
+        self.objetivos=([self.objetivos[((len(self.objetivos[:])-1)/2)][1]])
+        
+        print("Encontre una vez filtrados los puntos que este punto es el mejor: "+str(self.objetivos[0][1]))
+        
+        print(self.objetivos)
+        
+        if len(self.objetivos)!=0:
+
+            
+                
+                md.visualizacion_objetivos(self,self.objetivos,self.dato)
+        
+        
+
+
+        
+
+
+        print("Termine")
+        
+        
+        
+        
+         
+        
 
           
         #------------------------------------------------------------------
         
-    
+        
     
 if __name__ == "__main__":
     nodo=Nodo(Twist())
-    #rospy.Subscriber('/slam_out_pose',PoseStamped,nodo.posicion_robot_callback,queue_size=1)
+    rospy.Subscriber('/odom',Odometry,nodo.posicion_robot_callback,queue_size=1)
     rospy.Subscriber('/map',OccupancyGrid,nodo.map_callback,queue_size=100)
     rospy.spin()
 
