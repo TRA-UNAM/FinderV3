f290d998dc1fb3758d6d4b77fe968afb02745ebc
Axel Rojas <axeljavier13.rm@gmail.com>
2022-01-25 17:15:01 -0600

    Se trabaja en exploracion autonoma

--- catkin_ws/src/navigation/exploration/src/scripts/Servidor_Puntos_Frontera_FS.py
+++ catkin_ws/src/navigation/exploration/src/scripts/Servidor_Puntos_Frontera_FS.py
@@ -6,7 +6,7 @@ import rospy
 from nav_msgs.msg import OccupancyGrid
 from exploration.srv import Puntos_Frontera, Puntos_FronteraResponse
 import numpy as np
-import cv2 as cv
+import cv2
 
 class Servicio():
 
@@ -23,26 +23,26 @@ class Servicio():
         #c, l=np.shape(grafo)
         #grafo.flags.writeable = True
         grafo = np.uint8(grafo)
-        #cv.imshow("Result", grafo) 
-        x=cv.Sobel(grafo,cv.CV_16S,1,0)
-        y=cv.Sobel(grafo,cv.CV_16S,0,1)
-        absX = cv.convertScaleAbs(x)   # Transferencia de regreso a uint8  
-        absY = cv.convertScaleAbs(y) 
+        #cv2.imshow("Result", grafo) 
+        x=cv2.Sobel(grafo,cv2.CV_16S,1,0)
+        y=cv2.Sobel(grafo,cv2.CV_16S,0,1)
+        absX = cv2.convertScaleAbs(x)   # Transferencia de regreso a uint8  
+        absY = cv2.convertScaleAbs(y) 
         #print(absX[100,100])
-        bordes= cv.addWeighted(absX,0.2,absY,0.2,0)  
-        #bordes=cv.Canny(grafo.T,200,255)
-        ret,bordes=cv.threshold(bordes,90,255,cv.THRESH_BINARY)
+        bordes= cv2.addWeighted(absX,0.2,absY,0.2,0)  
+        #bordes=cv2.Canny(grafo.T,200,255)
+        ret,bordes=cv2.threshold(bordes,90,255,cv2.THRESH_BINARY)
         kernel = np.ones((2,2),np.uint8)
-        bordes=cv.dilate(bordes,kernel)
-        bordes=cv.erode(bordes,kernel)
-        bordes=cv.erode(bordes,kernel)
-        #bordes = cv.morphologyEx(bordes, cv.MORPH_CLOSE, kernel) 
+        bordes=cv2.dilate(bordes,kernel)
+        bordes=cv2.erode(bordes,kernel)
+        bordes=cv2.erode(bordes,kernel)
+        #bordes = cv2.morphologyEx(bordes, cv2.MORPH_CLOSE, kernel) 
         self.y=(np.where(bordes==255)[0])*req.resolution
         self.x=(np.where(bordes==255)[1])*req.resolution
         #print(x[0],y[0])
-        #cv.imshow("Result", bordes.T)  
-        #cv.waitKey(0)  
-        #cv.destroyAllWindows()
+        #cv2.imshow("Result", bordes.T)  
+        #cv2.waitKey(0)  
+        #cv2.destroyAllWindows()
         
           
         print("Ya termine de calcular los puntos frontera\n")
