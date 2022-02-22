ed7c859208e904a98ec8d12a632f2531a617bf1a
Axel Rojas <axeljavier13.rm@gmail.com>
2022-02-22 16:15:57 -0600

    Se modificaron cuestiones relacionadas al nodo de movimiento

--- catkin_ws/src/navigation/exploration/src/scripts/goal_point_server.py
+++ catkin_ws/src/navigation/exploration/src/scripts/goal_point_server.py
@@ -38,7 +38,7 @@ class Server:
         
         
         if abs(goal_x-req.last_objective.x)<0.05 and abs(goal_y-req.last_objective.y)<0.05:
-            (goal_x,goal_y)=heapq.heappop(h)[-1]
+            (goal_x,goal_y)=heapq.heappop(h)[1]
         
         
         goal=Point()
